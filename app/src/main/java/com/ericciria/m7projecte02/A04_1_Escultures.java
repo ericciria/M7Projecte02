a0077b814e9a08c8706adc469f960e4425170453
ericciria <ericciria_6624@hotmail.com>
2022-03-14 10:49:57 +0100

    Petits fix amb strings i el textview de A02

---
+++ app/src/main/java/com/ericciria/m7projecte02/A04_1_Escultures.java
@@ -0,0 +1,14 @@
+package com.ericciria.m7projecte02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class A04_1_Escultures extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a041_escultures);
+    }
+}
