a2853039255db729b2d168c78b461d77d0e20f86
ericciria <ericciria_6624@hotmail.com>
2022-03-17 09:40:18 +0100

    Acabat

--- app/src/main/java/com/ericciria/m7projecte02/A02_Fundacio.java
+++ app/src/main/java/com/ericciria/m7projecte02/A02_Fundacio.java
@@ -1,16 +1,21 @@
 package com.ericciria.m7projecte02;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class A02_Fundacio extends AppCompatActivity {
 
     TextView textDescripcio;
+    ImageButton ibText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,35 +24,36 @@ public class A02_Fundacio extends AppCompatActivity {
 
         textDescripcio = findViewById(R.id.tvTextInici);
         textDescripcio.setMovementMethod(new ScrollingMovementMethod());
+        ibText = findViewById(R.id.ibText);
 
     }
 
     public void anarAMapa (View v) {
         Intent mapejar = new Intent(this, A03_Mapa.class);
         startActivity(mapejar);
-
     }
 
     public void anarAArtistes (View x) {
         Intent artistejar = new Intent(this, A05_Artistes.class);
         startActivity(artistejar);
-
     }
 
     public void anarAEscultures (View y) {
-
         Intent escultpturar = new Intent(this, A04_Escultures.class);
         startActivity(escultpturar);
-
     }
 
     public void anarAFundcacio (View z) {
-
-
         Intent fundar = new Intent(this, A02_Fundacio.class);
         startActivity(fundar);
-
     }
 
-
+    public void textIdioma(MotionEvent event){
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                ibText.setColorFilter(R.color.Activat);
+            case MotionEvent.ACTION_UP:
+                ibText.setColorFilter(R.color.Desactivat);
+        }
+    }
 }
--- app/src/main/java/com/ericciria/m7projecte02/MainActivity.java
+++ app/src/main/java/com/ericciria/m7projecte02/MainActivity.java
@@ -4,14 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.Layout;
 import android.view.View;
+import android.window.SplashScreen;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final long SPLASH_TIEMPO = 4000;
+    View lTop;
+    View lMid;
+    View lBot;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lTop = findViewById(R.id.cTop);
+        lMid = findViewById(R.id.cMiddle);
+        lBot = findViewById(R.id.cBottom);
+
+        //lTop.displa
+
+        new Handler().postDelayed(new Runnable() {
+            /*
+             * Mostramos la pantalla de bienvenida con un temporizador.
+             * De esta forma se puede mostrar el logo de la app o
+             * compañia durante unos segundos.
+             */
+
+            @Override
+            public void run() {
+                // Este método se ejecuta cuando se consume el tiempo del temporizador.
+                // Se pasa a la activity principal
+                Intent i = new Intent(MainActivity.this, A02_Fundacio.class);
+                startActivity(i);
+
+                // Cerramos esta activity
+                finish();
+            }
+        }, SPLASH_TIEMPO);
+
+
 
 
 
