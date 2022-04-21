ec0188f9ee683e859f7755b0765e4de9ce112015
ericciria <ericciria_6624@hotmail.com>
2022-04-21 09:55:45 +0200

    Connectat a firebase i prova

--- app/src/main/java/com/ericciria/m7projecte02/A02_Fundacio.java
+++ app/src/main/java/com/ericciria/m7projecte02/A02_Fundacio.java
@@ -1,21 +1,37 @@
 package com.ericciria.m7projecte02;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class A02_Fundacio extends AppCompatActivity {
 
     TextView textDescripcio;
     ImageButton ibText;
+
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +42,31 @@ public class A02_Fundacio extends AppCompatActivity {
         textDescripcio.setMovementMethod(new ScrollingMovementMethod());
         ibText = findViewById(R.id.ibText);
 
+        String uids = "";
+        // Obtenim una instància d'accés a la base de dades Firestore.
+        db = FirebaseFirestore.getInstance();
+        Toast.makeText(A02_Fundacio.this, "" + db.collection("asd").get(), Toast.LENGTH_SHORT).show();
+        //Toast.makeText(A02_Fundacio.this, "" + db.collection("asd").document("asd1"), Toast.LENGTH_SHORT).show();
+
+        db.collection("asd")
+                .document("asd1")
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if (documentSnapshot.exists()) {
+                    Toast.makeText(A02_Fundacio.this, ""+ documentSnapshot.getData(), Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(A02_Fundacio.this, "No existeix", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                e.printStackTrace();
+                Toast.makeText(A02_Fundacio.this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void anarAMapa (View v) {
--- app/src/main/java/com/ericciria/m7projecte02/A05_1_Artistes.java
+++ app/src/main/java/com/ericciria/m7projecte02/A05_1_Artistes.java
@@ -1,10 +1,22 @@
 package com.ericciria.m7projecte02;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 public class A05_1_Artistes extends AppCompatActivity {
+
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
